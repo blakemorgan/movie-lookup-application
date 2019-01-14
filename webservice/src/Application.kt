78a6866f6bc1a0abbfc7668fe5d5a2596ec44bc1
Blake Morgan <b.morgan.296@gmail.com>
2019-01-13 17:18:56 -0700

    Get web service to work with gradle.

--- webservice/src/Application.kt
+++ webservice/src/Application.kt
@@ -1,64 +1,64 @@
-package com.blaketmorgan.ipapp.webservice
+package com.blaketmorgan.itapp.webservice
 
 import com.beust.klaxon.JsonObject
 import com.beust.klaxon.Parser
 import io.github.cdimascio.dotenv.dotenv
 import io.ktor.application.*
 import io.ktor.features.CORS
-import io.ktor.http.*
+import io.ktor.http.ContentType
 import io.ktor.response.*
-import io.ktor.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
+import io.ktor.routing.get
+import io.ktor.routing.routing
 import java.net.URL
 import java.net.URLEncoder
 
-fun main(args: Array<String>) {
-    val server = embeddedServer(Netty, port = 8081) {
-        install(CORS) {
-            anyHost()
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(CORS) {
+        anyHost()
+    }
+    routing {
+        get("/") {
+            call.respondText("Go to /movies to search movies", ContentType.Text.Plain)
         }
-        routing {
-            get("/") {
-                call.respondText("Go to /movies to search movies", ContentType.Text.Plain)
-            }
-            get("/movies") {
-                val query = call.request.queryParameters["search"] ?: ""
-                val response = queryTmdb(query)
+        get("/movies") {
+            val query = call.request.queryParameters["search"] ?: ""
+            val response = queryTmdb(query)
 
-                val parser = Parser()
-                val stringBuilder: StringBuilder = StringBuilder(response)
-                val json: JsonObject = parser.parse(stringBuilder) as JsonObject
-                val results = json.array<JsonObject>("results")
+            val parser = Parser()
+            val stringBuilder = StringBuilder(response)
+            val json: JsonObject = parser.parse(stringBuilder) as JsonObject
+            val results = json.array<JsonObject>("results")
 
-                var stringToReturn = "["
-                if (results != null) {
-                    val i = results.size;
-                    var iterate = 0
-                    for (result in results) {
-                        val movieId: Int = result["id"] as Int
-                        val title: String = "\"" + result["title"] + "\""
-                        val posterImageUrl: String =
-                            "\"https://image.tmdb.org/t/p/w92" + result["poster_path"] + "\""
-                        val popularitySumary: String =
-                            "\"" + result["popularity"] + " out of " + result["vote_count"] + "\""
-                        val movie =
-                            "{\"movie_id\": $movieId, \"title\": $title, \"poster_image_url\": $posterImageUrl, \"popularity_summary\": $popularitySumary}"
-                        stringToReturn += movie
-                        iterate++
-                        if (iterate != i && iterate != 10)
-                            stringToReturn += ","
-                        if (iterate == 10)
-                            break
-                    }
+            var stringToReturn = "["
+            if (results != null) {
+                val i = results.size
+                var iterate = 0
+                for (result in results) {
+                    val movieId: Int = result["id"] as Int
+                    val title: String = "\"" + result["title"] + "\""
+                    val posterImageUrl: String =
+                        "\"https://image.tmdb.org/t/p/w92" + result["poster_path"] + "\""
+                    val popularitySumary: String =
+                        "\"" + result["popularity"] + " out of " + result["vote_count"] + "\""
+                    val movie =
+                        "{\"movie_id\": $movieId, \"title\": $title, \"poster_image_url\": $posterImageUrl, \"popularity_summary\": $popularitySumary}"
+                    stringToReturn += movie
+                    iterate++
+                    if (iterate != i && iterate != 10)
+                        stringToReturn += ","
+                    if (iterate == 10)
+                        break
                 }
-                stringToReturn += "]"
-
-                call.respondText(stringToReturn)
             }
+            stringToReturn += "]"
+
+            call.respondText(stringToReturn)
         }
     }
-    server.start(wait = true)
 }
 
 fun queryTmdb(title: String?): String {
@@ -70,3 +70,4 @@ fun queryTmdb(title: String?): String {
 
     return URL(requestUrl).readText()
 }
+
